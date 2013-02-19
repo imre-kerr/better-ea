aa614e50888399eecb00feb2240de686239176c2
Imre Kerr <imre.kerr@gmail.com>
2013-02-19 21:29:26 +0100

    Binary genotypes are no longer objects but lists.
    Change: Binary genotypes are now lists, related functions updated to reflect this.

--- ea_binary_gtype.py
+++ ea_binary_gtype.py
@@ -1,40 +1,34 @@
 import random
 
-class binary_genotype:
-    def __init__(self, bits = []):
-        self.bits = bits # bits: list of 0s and 1s
-    
-    def generate(self, length):
-        self.bits = []
-        for i in xrange(length):
-            self.bits.append(random.randint(0, 1))
+def generate(length):
+    return [random.randint() for i in xrange(length)]
         
 def crossover(gene1, gene2, num_points):
-    points = random.sample(xrange(1, len(gene1.bits) - 1), num_points) + [len(gene1)]
-    crossed_bits = gene1.bits.copy()
+    points = random.sample(xrange(1, len(gene1) - 1), num_points) + [len(gene1)]
+    crossed_bits = gene1.copy()
     for start in xrange(0, len(points)-1, 2):
         end = start + 1
         for i in xrange(points[start], points[end]):
-            crossed_bits[i] = gene2_bits[i]
-    return binary_genotype(crossed_bits)
+            crossed_bits[i] = gene2[i]
+    return crossed_bits
         
 def gen_crossover():
     points = int(raw_input("Input no. of crossover points: "))
     return (lambda gene1, gene2: crossover(gene1, gene2, points))
     
 def bitwise_mutate(gene, chance):
-    mutated_bits = gene.bits.copy()
+    mutated_bits = gene.copy()
     for i in xrange(len(mutated_bits)):
         if random.random() <= chance:
             mutated_bits[i] = mutated_bits[i] ^ 1
-    return binary_genotype(mutated_bits)
+    return mutated_bits
     
 def genewise_mutate(gene, chance):
-    mutated_bits = gene.bits.copy()
+    mutated_bits = gene.copy()
     if random.random() <= chance:
         bit = random.randint(0, len(mutated_bits) - 1)
         mutated_bits[bit] = mutated_bits[bit] ^ 1
-    return binary_genotype(mutated_bits)
+    return mutated_bits
     
 def gen_mutate():
     while True:
