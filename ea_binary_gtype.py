0961d75ec5b10496b66616310f908fdce611178f
Imre Kerr <imre.kerr@gmail.com>
2013-02-19 21:33:39 +0100

    Docstrings
    Add: Docstrings in ea_binary_gtype.py

--- ea_binary_gtype.py
+++ ea_binary_gtype.py
@@ -1,9 +1,11 @@
 import random
 
 def generate(length):
+    '''Generate a random binary genotype'''
     return [random.randint() for i in xrange(length)]
         
 def crossover(gene1, gene2, num_points):
+    '''Create a new genotype by crossing two genotypes at num_points points, chosen randomly'''
     points = random.sample(xrange(1, len(gene1) - 1), num_points) + [len(gene1)]
     crossed_bits = gene1.copy()
     for start in xrange(0, len(points)-1, 2):
@@ -13,10 +15,12 @@ def crossover(gene1, gene2, num_points):
     return crossed_bits
         
 def gen_crossover():
+    '''Generate a crossover function'''
     points = int(raw_input("Input no. of crossover points: "))
     return (lambda gene1, gene2: crossover(gene1, gene2, points))
     
 def bitwise_mutate(gene, chance):
+    '''Mutate a genotype by flipping each bit with P=chance'''
     mutated_bits = gene.copy()
     for i in xrange(len(mutated_bits)):
         if random.random() <= chance:
@@ -24,6 +28,7 @@ def bitwise_mutate(gene, chance):
     return mutated_bits
     
 def genewise_mutate(gene, chance):
+    '''Mutate a genotype by flipping a single bit with P=chance'''
     mutated_bits = gene.copy()
     if random.random() <= chance:
         bit = random.randint(0, len(mutated_bits) - 1)
@@ -31,6 +36,7 @@ def genewise_mutate(gene, chance):
     return mutated_bits
     
 def gen_mutate():
+    '''Generate a mutation function'''
     while True:
         if method == 'bitwise':
             chance = float(raw_input("Input per-bit mutation rate: "))
