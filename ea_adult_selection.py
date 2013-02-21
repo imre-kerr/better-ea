ea6e7b41a551ab43cd808c66fb60706719c41b98
Imre Kerr <imre.kerr@gmail.com>
2013-02-21 01:13:21 +0100

    Fix: Adult selection no longer discards fitness values

--- ea_adult_selection.py
+++ ea_adult_selection.py
@@ -1,17 +1,17 @@
 def full_generational_replacement(population):
     '''Cull all individuals that are more than 1 generation old'''
-    return [(ind[0], ind[1], ind[3]) for ind in population if ind[3] == 0]
+    return [ind for ind in population if ind[3] == 0]
 
 def overproduction(population, n):
     '''Cull all but the n most fit individuals that are no more than 1 generation old'''
     newpop = [ind for ind in population if ind[3] == 0]
     newpop.sort(key=(lambda ind: ind[2]), reverse=True)
-    return [(ind[0], ind[1], ind[3]) for ind in newpop[:n]]
+    return [ind for ind in newpop[:n]]
 
 def generational_mixing(population, n):
     '''Cull all but the n most fit individuals'''
     newpop = population.sorted(key=(lambda ind: ind[2]), reverse=True)
-    return [(ind[0], ind[1], ind[3]) for ind in newpop[:n]]
+    return [ind for ind in newpop[:n]]
 
 def gen_adult_selection(popsize):
     '''Generate an adult selection function interactively'''
--- ea_main.py
+++ ea_main.py
@@ -11,13 +11,13 @@ def evolutionary_algorithm(initial, development, fitness_test,
                            reproduction, generations, fitness_goal):
     '''Main EA loop. 
     
-    Returns a list of all generations, each a list of (gtype, ptype, fitness) touples'''
+    Return a list of all generations, each a list of (gtype, ptype, fitness) touples'''
     generation_list = [] # generation_list: [[(gtype, ptype, fitness) list] list]
     genotypes = initial # genotypes: [(gtype, age) list]
     for gen in xrange(generations):
         developed_population = develop(genotypes) # developed_population: [(gtype, ptype, age) list]
         tested_population = fitness_test(developed_population) # tested_population: [(gtype, ptype, fitness, age) list]
-        culled_population = adult_selection(tested_population) # culled_population: [(gtype, ptype, age) list]            
+        culled_population = adult_selection(tested_population) # culled_population: [(gtype, ptype, fitness, age) list]            
         generation_list += culled_population
         if done(culled_population, fitness_goal):
             break
