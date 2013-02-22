efcfd68d6572ed5bb733cd38fdc5fd2ffa2d3a68
Imre Kerr <imre.kerr@gmail.com>
2013-02-22 01:09:14 +0100

    Change: axed the idea of an autogen script

--- problems/onemax.py
+++ problems/onemax.py
@@ -1,4 +1,6 @@
-from ea import autogen
+from ea import adult_selection
+from ea import parent_selection
+from ea import reproduction
 from ea import main
 from ea import binary_gtype
 
@@ -12,9 +14,21 @@ def develop(population):
 
 def visualize(generation_list):
     '''Generate visualizations using matplotlib'''
+    return None
 
 if __name__=='__main__':
     size = int(raw_input("Input problem size: "))
-    popsize, adult_selection, parent_selection, reproduction, generations, fitness_goal = autogen.generate()
+    popsize = int(raw_input("Input population size: "))
+
+    adult_selection = adult_selection.generate()
+    parent_selection = parent_selection.generate()
+
+    mutate = binary_gtype.gen_mutate()
+    crossover = binary_gtype.gen_crossover()
+    reproduction = reproduction.gen_reproduction(mutate, crossover)
+
+    generations = int(input("Input max number of generations: "))
+    fitness_goal = float(input("Input fitness goal, 0 for none: "))
+
     initial = [binary_gtype.generate(size), 0 for i in xrange(popsize)]
     generation_list = main.evolutionary_algorithm(initial, develop, fitness_test, adult_selection, parent_selection, reproduction, generations, fitness_goal)
