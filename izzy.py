16132aa0e2b2c05bee9b24d1323233390857fdf1
Imre Kerr <imre.kerr@gmail.com>
2013-02-22 19:58:46 +0100

    Change: spiketrain now generates steps points and not steps+1

--- izzy.py
+++ izzy.py
@@ -4,16 +4,16 @@ def spiketrain(a, b, c, d, k, tau, ext_input, thresh, steps):
     '''Compute a spike train according to the Izhikevich model'''
     v = -60
     u = 0
-    train = [v]
+    train = []
     for i in xrange(steps):
-        dv = 1/tau * (k * v**2 + 5*v + 140 - u + ext_input[i])
-        du = a/tau * (b*v - u)
-        v += dv
-        u += du
         train += [v]
         if v >= thresh:
             v = c
             u = u + d
+        dv = 1/tau * (k * v**2 + 5*v + 140 - u + ext_input[i])
+        du = a/tau * (b*v - u)
+        v += dv
+        u += du
     return train
 
 def detect_spikes(spike_train, thresh, k):
