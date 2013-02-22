050e203d514a01c4e3fe7e9c521364f3832d7409
Imre Kerr <imre.kerr@gmail.com>
2013-02-22 20:24:24 +0100

    Add: Spike interval distance metric

--- izzy.py
+++ izzy.py
@@ -31,14 +31,31 @@ def dist_spike_time(train1, train2):
     '''Compute distance between two spike trains using the spike time distance metric'''
     spikes1 = detect_spikes(train1)
     spikes2 = detect_spikes(train2)
-
+    
     n = min(len(spikes1), len(spikes2))
     p = 2
-
-    dist = 1/n * sum(abs(spikes1[i] - spikes2[i])**2 for i in xrange(n)) ** (1/p)
-
+    
+    dist = 1/n * sum(abs(spikes1[i] - spikes2[i])**p for i in xrange(n)) ** (1/p)
+    
     # Note that m and n are reversed in relation to their names in izzy-evo.pdf
     m = max(len(train1), len(train2))
     penalty = (m - n) * len(train1) / (2 * n)
-
+    
     return dist + penalty
+
+def dist_spike_interval(train1, train2):
+    '''Compute distance between two spike trains using the spike interval distance metric'''
+    spikes1 = detect_spikes(train1)
+    spikes2 = detect_spikes(train2)
+    
+    n = min(len(spikes1), len(spikes2))
+    p = 2
+    
+    dist = 1/(n-1) * sum(abs((spikes1[i] - spikes1[i-1])-(spikes2[i] - spikes2[i-1]))**p for i in xrange(1,n)) ** (1/p)
+    
+    # Note that m and n are reversed in relation to their names in izzy-evo.pdf
+    m = max(len(train1), len(train2))
+    penalty = (m - n) * len(train1) / (2 * n)
+    
+    return dist + penalty
+
