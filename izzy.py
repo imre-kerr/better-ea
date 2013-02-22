2c3354225b391b7c055894a5950e459d60616504
Imre Kerr <imre.kerr@gmail.com>
2013-02-22 20:08:02 +0100

    Change: detect_spikes no longer takes thresh and window size as args

--- izzy.py
+++ izzy.py
@@ -16,11 +16,15 @@ def spiketrain(a, b, c, d, k, tau, ext_input, thresh, steps):
         u += du
     return train
 
-def detect_spikes(spike_train, thresh, k):
+def detect_spikes(spike_train):
     '''Detect spikes in a spike train using a sliding window of size k'''
+    thresh = 0
+    k = 5
     spikes = []
     for i in xrange(len(spike_train) - k + 1):
         window = spike_train[i:i+k]
         if window[k/2] == max(window) and window[k/2] > thresh:
-            spikes += [k/2]
+            spikes += [window[k/2]]
     return spikes
+
+    
