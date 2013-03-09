9b9bc7a0648b0a296d210e3636538f56914de3b8
Imre Kerr <imre.kerr@gmail.com>
2013-03-09 19:47:55 +0100

    Add: Game file code skeleton

---
+++ min_cog_game.py
@@ -0,0 +1,16 @@
+class Game:
+    def __init__(self):
+        '''Generate a game instance that can be played by several agents in turn.
+        
+        Order of blocks should be decided here.'''
+        
+        # CODE GOES HERE
+        
+    def play(self, ctrnn, visual):
+        '''Plays a game with the CTRNN argument as the controller.
+        
+        Object's internal state should NOT change between successive calls.
+        If visual is True, a visualization of the gameplay should be shown.'''
+        
+        # MORE CODE HERE
+        
