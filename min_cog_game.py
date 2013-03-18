e818c2bead835012285f7411f81a99b028e4cb0f
Imre Kerr <imre.kerr@gmail.com>
2013-03-18 16:38:35 +0100

    Add: Visualization!

--- min_cog_game.py
+++ min_cog_game.py
@@ -1,4 +1,11 @@
+import pygame
+import random
+
 class Game:
+    board_width = 30
+    board_height = 15
+    block_size = 30
+
     def __init__(self):
         '''Generate a game instance that can be played by several agents in turn.
         
@@ -6,11 +13,56 @@ class Game:
         
         # CODE GOES HERE
         
+        
+    def visual_init(self):
+        '''Initialize some pygame stuff'''
+        pygame.init()
+        self.fps_clock = pygame.time.Clock()
+    
+        self.window_surface_obj = pygame.display.set_mode((Game.board_width*Game.block_size, Game.board_height*Game.block_size))
+        pygame.display.set_caption("Minimally Cognitive Agent: The Game: The Movie")
+        
+        self.black_color = pygame.Color(0,0,0)
+        self.red_color = pygame.Color(255,0,0)
+        self.blue_color = pygame.Color(0,0,255)
+        
+        self.font_obj = pygame.font.Font('freesansbold.ttf', 32)
+        
+        
+    def visual_frame(self):
+        '''Draw a single frame and sync to 3 FPS'''
+                
+        for x in xrange(Game.board_width):
+            for y in xrange(Game.board_height):
+                # cell = self.board[x][y]
+                cell = random.choice((0,1,2))
+                color = None
+                if cell == 0:
+                    color = self.black_color
+                elif cell == 1:
+                    color = self.blue_color
+                else:
+                    color = self.red_color
+                pygame.draw.rect(self.window_surface_obj, color, pygame.Rect(x*Game.block_size, y*Game.block_size, Game.block_size, Game.block_size))
+                
+        pygame.display.update()
+        self.fps_clock.tick(3)
+        
+ 
     def play(self, ctrnn, visual):
         '''Plays a game with the CTRNN argument as the controller.
         
         Object's internal state should NOT change between successive calls.
         If visual is True, a visualization of the gameplay should be shown.'''
         
-        # MORE CODE HERE
+        if visual:
+            self.visual_init()
         
+        num_timesteps = 100
+        for step in xrange(num_timesteps):
+            # REAL STUFF
+            
+            if visual:
+                self.visual_frame()
+                
+        pygame.quit()
